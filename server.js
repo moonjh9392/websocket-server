20c4516f69a0f8a564f8419c4d3cc4980400f3a2
butter-moonjh <151711779+butter-moonjh@users.noreply.github.com>
2024-04-05 17:55:28 +0900

    커밋

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
-const WebSocket = require("ws");
 const cors = require("cors");
 const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+
 const app = express();
 
 // 모든 출처에서의 요청을 허용
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
-const server = app.listen(8080);
+// HTTP 서버 생성
+const server = http.createServer(app);
 
-const wss = new WebSocket.Server({
-  server,
-});
+// 웹소켓 서버 생성 및 구성
+const wss = new WebSocket.Server({ server });
 
 wss.on("connection", function connection(ws) {
   console.log("클라이언트가 연결되었습니다.");
 
   ws.on("message", function incoming(message) {
     console.log("받은 메시지:", message);
+    ws.send("서버에서 클라이언트로 메시지를 보냅니다.");
   });
-
-  ws.send("서버에서 클라이언트로 메시지를 보냅니다.");
 });
 
-console.log("웹소켓 서버가 포트 8080에서 실행중입니다.");
+// 서버 리스닝 시작
+server.listen(8080, "0.0.0.0", function () {
+  console.log("서버가 포트 8080에서 실행중입니다.");
+});
